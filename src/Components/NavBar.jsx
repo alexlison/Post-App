57cdf87ff5a7c852b63c88a4810ceb0ca419d454
alexlison <alexlison910@gmail.com>
2025-07-13 17:42:41 +0530

    Post-App Project Updated

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,8 +4,7 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
     <div>
-      <div className="container">
-        <div className="row">
+
           <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
             <nav class="navbar navbar-fluid navbar-expand-lg bg-info">
               <div class="container-fluid">
@@ -51,8 +50,7 @@ const NavBar = () => {
             </nav>
           </div>
         </div>
-      </div>
-    </div>
+
   );
 };
 
